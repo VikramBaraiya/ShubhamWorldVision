15930b3d15027a456b62c0b1e74ac5e6b31e6d06
VikramBaraiya <contactbaraiya@gmail.com>
2019-02-12 17:01:34 -0500

    Added a layout file to show customer data in recycler view only layout file has been added it is not implemented nor designed

---
+++ app/src/main/java/yadunandan/shubham/CustomerDataLayout.java
@@ -0,0 +1,13 @@
+package yadunandan.shubham;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerDataLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_data_layout);
+    }
+}
